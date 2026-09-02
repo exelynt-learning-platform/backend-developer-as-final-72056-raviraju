cc2d061e44a057bd7b9e2df8938c60e73756eebc
Ravi Raju <reachraviraju@gmail.com>
2026-09-02 12:47:32 +0530

    Initial Spring Boot project skeleton

---
+++ resource-booking-api/src/main/java/com/raviraju/resource_booking_api/ResourceBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.raviraju.resource_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceBookingApiApplication.class, args);
+	}
+
+}
